25d8bbe9f9eac84f2f27b4262fb98969fd8bea59
SickNB <provaazzurro2@outlook.it>
2025-05-08 10:17:54 +0200

    Primo commit

---
+++ main.tf
@@ -0,0 +1,113 @@
+provider "azurerm" {
+  features {}
+  subscription_id = "561db2f4-9124-4b23-8817-f6bada0675f5"
+}
+resource "azurerm_resource_group" "rg" {
+  name     = "Rg-Progetto-Finale"
+  location = "westeurope"
+}
+resource "azurerm_virtual_network" "vnet" {
+  name                = "Vnet-Lab-Finale"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+resource "azurerm_subnet" "subnet" {
+  name                 = "Subnet-Lab-Finale"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+resource "azurerm_public_ip" "public_ip" {
+  count               = 2
+  name                = "public-ip-${count.index}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Static"
+  sku                 = "Basic"
+}
+resource "azurerm_network_interface" "nic" {
+  count               = 2
+  name                = "nic-${count.index}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "ipconfig"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.public_ip[count.index].id
+  }
+}
+resource "azurerm_linux_virtual_machine" "vm" {
+  count               = 1
+  name                = "lab-acc-${count.index}"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = "Standard_B2ms"
+  admin_username      = "adam"
+  admin_password      = "123.Comodino"
+  disable_password_authentication = false
+
+  network_interface_ids = [
+    azurerm_network_interface.nic[count.index].id
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts-gen2"
+    version   = "latest"
+  }
+}
+resource "azurerm_network_security_group" "nsg" { 
+  name                = "Nsg-Lab-Finale"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "AllowSSH"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  } 
+
+
+
+security_rule {
+  name                       = "AllowNodePort"
+  priority                   = 1002
+  direction                  = "Inbound"
+  access                     = "Allow"
+  protocol                   = "Tcp"
+  source_port_range          = "*"
+  destination_port_range     = "30000-32767"
+  source_address_prefix      = "*"
+  destination_address_prefix = "*"
+}
+
+security_rule {
+  name                       = "AllowHTTP"
+  priority                   = 1003
+  direction                  = "Inbound"
+  access                     = "Allow"
+  protocol                   = "Tcp"
+  source_port_range          = "*"
+  destination_port_range     = "80"
+  source_address_prefix      = "*"
+  destination_address_prefix = "*"
+} 
+
+
+
+} 
---
+++ variables.tf
@@ -0,0 +1,28 @@
+
+ variable "resource_group_name" {
+  default = "Rg-Progetto-Finale"
+}
+
+variable "location" {
+  default = "westeurope"
+}
+
+variable "vnet_name" {
+  default = "Vnet-Lab-Finale"
+} 
+
+variable "subnet_name" {
+  default = "Subnet-Lab-Finale"
+}
+
+variable "nsg_name" {
+  default = "Nsg-Lab-Finale"
+}
+
+variable "admin_username" {
+  default = "adam"
+}
+
+variable "admin_password" {
+  default = "123.Comodino" 
+}
